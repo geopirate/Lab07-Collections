cbff9ac9c7e1bf2ffbbdf0b47d4c0ecf3bf898b2
George Gaylor <georgegaylor83@outlook.com>
2017-09-20 22:15:44 -0700

    rough delete

--- Lab07George/Lab07George/Inventory.cs
+++ Lab07George/Lab07George/Inventory.cs
@@ -7,29 +7,71 @@ namespace Lab07George
 {
     class Inventory<T> : IEnumerable<T>
     {
-        T[] inventory = new T[20];
+        T[] product = new T[20];
         int counter = 0;
 
         public void Add(T item)
         {
-            if (counter == inventory.Length)
+            if (counter == product.Length)
             {
-                Array.Resize(ref inventory, inventory.Length * 2);
+                Array.Resize(ref product, product.Length * 2);
+                // int index = count * 2;
+                // T[] extension = new T[index];
+                // Array.Copy(students, extension, index);
+                // students = extension;
             }
-            inventory[counter++] = item;
+            product[counter++] = item;
+            // students[counter] =student;
+            // counter++;
         }
 
         public void Remove(T item)
         {
-            //inventory GetEnumerator()
-            Console.WriteLine("Sorry Lynn, This is not removing anything.");
+            Console.WriteLine($"Deleting {item}");
+            int removeAt = Array.IndexOf(product, item);
+            bool remove = false;
+            T[] newArray = new T[product.Length];
+
+            for (int i = 0; i < counter; i++)
+            {
+                if(i == removeAt)
+                {
+                    remove = true;
+                }
+                else if (remove)
+                {
+                    newArray[i - 1] = product[i];
+                }
+                else
+                {
+                    newArray[i] = product[i];
+                }
+            }
+            product = newArray;
+            counter--;
+        }
+
+        public void ViewAll()
+        {
+            /*foreach (Product p in this)
+            {
+                Console.WriteLine(p.Name + " is a " + p.ProductType);
+            }
+            Console.Read();*/
+            
+
+
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine($"Thing {product[i].Name}");
+            }
         }
 
         public IEnumerator<T> GetEnumerator()
         {
             for (int i = 0; i < counter; i++)
             {
-                yield return inventory[i];
+                yield return product[i];
             }
         }
 
--- Lab07George/Lab07George/Program.cs
+++ Lab07George/Lab07George/Program.cs
@@ -10,24 +10,41 @@ namespace Lab07George
             Product soap = new Product("soap") { ProductType = Category.Cosmetics };
             Product playstation = new Product("playstation") { ProductType = Category.Electronics };
             Product shampoo = new Product("shampoo") { ProductType = Category.Cosmetics };
+            Product broom = new Product("broom") { ProductType = Category.Housewares };
+            Product mop = new Product("mop") { ProductType = Category.Housewares };
+            Product towel = new Product("towel") { ProductType = Category.Housewares };
+            Product apple = new Product("apple") { ProductType = Category.Grocery };
+            Product melon = new Product("melon") { ProductType = Category.Grocery };
+            Product xbox = new Product("xbox") { ProductType = Category.Electronics };
+            Product hammer = new Product("hammer") { ProductType = Category.Hardware };
+            Product screwdriver = new Product("screwdriver") { ProductType = Category.Hardware };
 
             Console.WriteLine("This is my collection of products.");
-
-            //Console.WriteLine(Category.Electronics);
 
             Inventory<Product> myProducts = new Inventory<Product>()
             {
                 soap,
-                playstation
+                playstation,
+                xbox,
+                hammer,
+                screwdriver,
+                apple,
+                melon
             };
             myProducts.Add(shampoo);
+            myProducts.Add(broom);
+            myProducts.Add(mop);
+            myProducts.Add(towel);
 
-            myProducts.Remove(soap);
-
-            foreach(Product p in myProducts)
+            foreach (Product p in myProducts)
             {
                 Console.WriteLine(p.Name + " is a " + p.ProductType);
             }
+
+            myProducts.Remove(soap);
+
+            myProducts.ViewAll();
+
             Console.Read();
         }
     }
@@ -39,5 +56,5 @@ namespace Lab07George
  * * Make sure the generic has the functionality to use a foreach loop.
  * Your generic collection should Add(), Remove() items in the collection
  * Stretch - if there is time, make it so you can ViewAll and create a method to output all items to the console
- * Add at least 10 items to your List, and output them to the console. 
- * Add some items through the Add() Method, and others through a collection initializer */
+ * * Add at least 10 items to your List, and output them to the console. 
+ * * Add some items through the Add() Method, and others through a collection initializer */
